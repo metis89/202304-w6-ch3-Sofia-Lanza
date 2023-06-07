6f54832f7f87adb7216a704d6eecac7b0c971590
Sofia Hudson <sofia-sama@hotmail.com>
2023-06-07 20:44:02 +0200

    Add and fix functionalities

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./core/components/App";
-import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./core/store/store";
+import "./style.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
